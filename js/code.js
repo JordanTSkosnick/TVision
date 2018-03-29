26422321b2cc7a81adf872d72d3d1d36aee3f184
themarineelite <37886810+themarineelite@users.noreply.github.com>
2018-03-28 20:46:40 -0400

    Update by Hank 3/28/2018
    Added shows to recShows

--- js/code.js
+++ js/code.js
@@ -1,6 +1,14 @@
 //Jordan Skosnick
 //API KEY = a082f301
-var recShows = ['Game Of Thrones','Breaking Bad'];
+var recShows = ['Planet Earth 2', 'Band of Brothers', 'Planet Earth', 'Game Of Thrones', 'Breaking Bad', 'The Wire', 
+		'Rick and Morty', 'Cosmos: A Spacetime Odyssey', 'Blue Planet 2', 'Cosmos', 'The World at War', 'The Sopranos', 
+		'Life', 'Avatar: The Last Airbender', 'Sherlock', 'Human Planet', 'The Civil War', 'The Twilight Zone', 'Dekalog',
+	        'Firefly', 'True Detective', 'Fullmetal Alchemist: Brotherhood', 'Last Week Tonight with John Oliver', 'Fargo',
+	        'Batman: The Animated Series', 'Death Note', 'The Blue Planet', 'One Punch Man', 'Cowboy Bebop', 'Frozen Planet',
+	        'Black Mirror', "Monty Python's Flying Circus", 'Pride and Prejudice', 'Africa', 'Stranger Things', 'Das Boot',
+	        'Westworld', 'Arrested Development', 'House of Cards', 'Friends', 'Seinfield', 'Only Fools and Horses....',
+	        'Over the Garden Wall', 'TVF Pitchers', 'Twin Peaks', 'Narcos', 'Freaks and Geeks', 'I, Claudius', 'Gravity Falls',
+	        'Fawlty Towers'];
 //Creates an on submit listener for the search bar on the index page.
 $(document).ready(function() {
   $('#searchForm').on('submit', function(e) {
